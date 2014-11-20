32d011893077f16da54b35138781c61778965e61
SMB開発部 <sorimachi-aws@hotmail.com>
2014-11-20 11:00:21 +0900

    Initial Commit

---
+++ chapter8-4(2nd)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chapter8-4(2nd)
+//
+//  Created by SMB開発部 on 2014/11/20.
+//  Copyright (c) 2014年 Sorimachi,corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
